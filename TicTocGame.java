722b6f27ff5dc96fad97f2da4f425ee0b2e64d14
dewmal <dewmalnilanka@gmail.com>
2018-10-14 11:26:21 +0530

    Game Play with win pos

--- TicTocGame.java
+++ TicTocGame.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
 
-
+/**
+Author : Dewmal dewmalnilanka@gmail.comm
+Date : 2018.10.14
+**/
 public class TicTocGame {
 
 	//Game Screen Details
-	private int gameBoardNumberOfRows=3;
-	private int gameBoardNumberOfColumns=3;
-
-
+	private int gameBoardNumberOfRows=4;
+	private int gameBoardNumberOfColumns=gameBoardNumberOfRows;
 	private char gameBoard[][]=new char[gameBoardNumberOfRows][gameBoardNumberOfColumns];
 	private boolean isGameEnd = false;
-	private char gameDefaultMark='-';
+	private char gameDefaultMark='-'; // Check to set 
 
 	// Iinitiate Player Array
-	private int numberOfPlayers=2;
+	private int numberOfPlayers=3;
 	private String players[]=new String[numberOfPlayers];
-	private char playSymbols[]=new char[]{'X','O'};
+	private char playSymbols[]=new char[]{'X','O','Y'};
 
 	// Getting Keyboard inputs
 	private Scanner keyboard= new Scanner(System.in);
 
 
 	TicTocGame(){
-		initiateGameScreen();
+		initiateGameScreen(); // Initiate Game Screen Here
 	}
 
-
+	/**
+	Show Game Screen
+	**/
 	private void initiateGameScreen(){
 		for (int r=0;r<gameBoardNumberOfRows;r++ ) {			
 			for (int c=0;c<gameBoardNumberOfColumns;c++ ) {
@@ -35,10 +38,15 @@ public class TicTocGame {
 	}
 		
 
-
+	/**
+	Print Game Screen In Console
+	**/
 	private void printGameScreen(){
 		for (int r=0;r<gameBoardNumberOfRows;r++ ) {
-			System.out.println(" --- --- ---");
+			for (int k=0;k<gameBoardNumberOfColumns;k++ ) {
+				System.out.print(" ---");
+			} 
+			System.out.println();
 			for (int c=0;c<gameBoardNumberOfColumns;c++ ) {
 				char gameCell =gameBoard[r][c];				
 				System.out.print(" :"+gameCell+":");				
@@ -61,10 +69,98 @@ public class TicTocGame {
 		return true;
 	}
 
+	/**
+		Get player name belong to given Index
+	**/
 	private String getPlayerName(int currentPlayerIndex){
 		return players[currentPlayerIndex];
 	}
 
+
+
+	private boolean checkIsPlayerWin(char playerMark){
+		boolean isWin=false;
+		boolean rowCheck=false;
+		boolean colCheck=false;
+		boolean diagonalCheck1=true;
+		boolean diagonalCheck2=true;
+		int nextMarkPos=0;
+
+		
+
+
+		// Row Check
+		L1:for (int r=0;r<gameBoardNumberOfRows;r++ ) {
+			boolean isRowHasSame=true;
+
+			for (int c=0;c<gameBoardNumberOfColumns;c++ ) {
+				char boardMark=gameBoard[r][c];
+				if(boardMark==playerMark){
+					isRowHasSame&=isRowHasSame;
+				}else{
+					isRowHasSame=false;
+				}
+
+			}
+
+			rowCheck=isRowHasSame;	
+
+			if(rowCheck){
+				break L1;	
+			}	
+		}
+
+
+		// Col Check
+		L2:for (int r=0;r<gameBoardNumberOfRows;r++ ) {
+			boolean isColHasSame=true;
+			for (int c=0;c<gameBoardNumberOfColumns;c++ ) {
+				char boardMark=gameBoard[c][r];
+				if(boardMark==playerMark){
+					isColHasSame&=isColHasSame;
+				}else{
+					isColHasSame=false;
+				}
+			}
+			colCheck=isColHasSame;	
+			if(colCheck){
+				break L2;	
+			}	
+		}
+
+		// Diag 1 Check
+		L3:for (int r=0;r<gameBoardNumberOfRows;r++ ) {			
+			for (int c=0;c<gameBoardNumberOfColumns;c++ ) {
+				if (c==r){
+					char boardMark=gameBoard[r][c];
+					//System.out.print("boardMark"+boardMark);
+					if(boardMark==playerMark){
+						diagonalCheck1&=diagonalCheck1;
+					}else{
+						diagonalCheck1=false;
+					}
+				}				
+			}
+		}
+
+		// Diag 2 Check
+		for (int r=0;r<gameBoardNumberOfRows;r++ ) {			
+			for (int c=0;c<gameBoardNumberOfColumns;c++ ) {
+				int x=c;
+				int y=gameBoardNumberOfColumns-1-r;
+				if (x==y){
+					char boardMark=gameBoard[r][c];					
+					if(boardMark==playerMark){
+						diagonalCheck2&=diagonalCheck2;
+					}else{
+						diagonalCheck2=false;
+					}
+				}				
+			}
+		}
+
+		return rowCheck||colCheck||diagonalCheck1||diagonalCheck2;
+	}
 	/**
 	Game Loop run here
 	**/
@@ -110,22 +206,33 @@ public class TicTocGame {
 				int posX=Integer.parseInt(pos[0]+"");
 				int posY=Integer.parseInt(pos[2]+"");
 
-				char gameMark=gameBoard[posX-1][posY-1];
-
-				if (gameMark==gameDefaultMark){
-					gameBoard[posX-1][posY-1]=playerMark;
-					isSuccessInput=true;
+				if (posX>0 && posY>0 && posX<=gameBoardNumberOfRows&&posY<=gameBoardNumberOfColumns){
+					char gameMark=gameBoard[posX-1][posY-1];
+					if (gameMark==gameDefaultMark){
+						gameBoard[posX-1][posY-1]=playerMark;
+						isSuccessInput=true;
+					}else{
+						System.out.println("You can't Insert Marks to That Position");
+					}	
 				}else{
-					System.out.println("You can't Insert Marks to That Position");
+					System.out.println("Invalid Position");
 				}
 
-				
+						
 
 			}while(!isSuccessInput);
+			
+			// Check Winner
+			boolean isCurruntPlayerWin=checkIsPlayerWin(playerMark);
 			
 
 			// Show game screen & Result
 			printGameScreen();
+
+			if (isCurruntPlayerWin){
+				System.out.println("Player "+playerName+" Win");
+				isGameEnd=true;
+			}
 
 			// Update Current Player to next player
 			currentPlayer++;
@@ -133,6 +240,8 @@ public class TicTocGame {
 			
 
 		}
+
+		System.out.println("Game End");
 
 	}
 
